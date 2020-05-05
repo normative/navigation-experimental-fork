e25f62ab55e046ac527806618a83c64caef3d31f
tonybennett <anthony@exhibit-x.com>
2020-05-05 15:10:26 -0400

    replace View.propTypes, bump version

--- NavigationExperimentalComponents/NavigationCardStack.js
+++ NavigationExperimentalComponents/NavigationCardStack.js
@@ -31,7 +31,7 @@
  * @flow
  */
 'use strict';
-import { NativeModules } from 'react-native';
+import { NativeModules, ViewPropTypes } from 'react-native';
 const NativeAnimatedModule = NativeModules.NativeAnimatedModule;
 const NavigationCard = require('./NavigationCard');
 const NavigationCardStackPanResponder = require('./NavigationCardStackPanResponder');
@@ -200,12 +200,12 @@ class NavigationCardStack extends React.Component<DefaultProps, Props, void> {
     /**
      * Custom style applied to the cards stack.
      */
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
 
     /**
      * Custom style applied to the scenes stack.
      */
-    scenesStyle: View.propTypes.style,
+    scenesStyle: ViewPropTypes.style,
   };
 
   static defaultProps: DefaultProps = {
